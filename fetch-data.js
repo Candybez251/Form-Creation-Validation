3a46c05a6961641c23fcf68276d033c036cfb056
candybez251 <kalkidanblemma@gmail.com>
2024-07-30 16:49:36 +0300

    add 3 fetch files

--- fetch-data.js
+++ fetch-data.js
@@ -1,31 +1,26 @@
-async function fetchUserData()  {
-    const apiurl = 'https://jsonplaceholder.typicode.com/users';
-    const dataContainer = document.getElementById('api-data'); 
-    
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data');
+
     try {
-        const response = await fetch(apiurl);
+        const response = await fetch(apiUrl);
         const users = await response.json();
 
         dataContainer.innerHTML = '';
+
         const userList = document.createElement('ul');
-
         users.forEach(user => {
-            
             const listItem = document.createElement('li');
-
             listItem.textContent = user.name;
-            
             userList.appendChild(listItem);
         });
+
         dataContainer.appendChild(userList);
-
-    }
-
-    catch (error) {
+    } catch (error) {
         dataContainer.innerHTML = '';
-        dataContainer.innerHTML = 'Failed to load user data.';
-        dataContainer.classList.add('error');  
+        dataContainer.textContent = 'Failed to load user data.';
+        dataContainer.classList.add('error');
     }
-
 }
+
 document.addEventListener('DOMContentLoaded', fetchUserData);
